c558a226011e5cdb3af8e6792bd39b1756db14f9
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-09 19:03:03 +0300

    register models and create models , finally install Pillow

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=60)
+    image_caption=models.TextField()
+    likes=models.IntegerField(default=0)
+    comments=models.TextField()
+    image = models.ImageField(upload_to='images/',blank=True)
+    profile=models.ForeignKey('Profile',on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='profile_pics')
+    bio = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
