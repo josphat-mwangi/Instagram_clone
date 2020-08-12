f810fada8685779df3d1fcc53dcb1aa3b6021bc0
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-12 22:33:45 +0300

    add comment form and postform

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile,Image
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -21,3 +21,12 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_photo','bio']
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('image','image_caption')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['image', 'image_caption',' image_name',]
