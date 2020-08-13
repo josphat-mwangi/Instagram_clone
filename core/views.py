49a9a5ceda4d03e6a6d8d1908b01f7dcd292e704
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-13 14:51:55 +0300

    add views for post

--- core/views.py
+++ core/views.py
@@ -4,7 +4,14 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserUpdateForm,UserRegistrationForm,ProfileUpdateForm,CommentForm,PostForm
 from django.contrib.auth.models import User
 from.models import Image,Posts,Comments
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # Create your views here.
@@ -62,11 +69,52 @@ def search(request):
         message = 'Enter term to search'
         return render(request, 'search.html', {'message':message})
 
+
 class PostListView(ListView):
     model = Post
-    template_name = 'posts/post_list.html'
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    ordering = ['-created_date']
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 
 
 @login_required (login_url='/accounts/register/')
